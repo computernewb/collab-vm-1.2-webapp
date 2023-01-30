3c17e457d64589665b7ffe6e7c4ea0afef5e1c80
modeco80 <lily.modeco80@protonmail.ch>
2023-01-29 19:59:45 -0500

    Working Babel transpiling

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
 const path = require("path");
+
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    mode: "production"
+	entry: "./src/index.js",
+
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+
+	module: {
+	  rules: [
+	    {
+	      test: /\.m?js$/,
+	      exclude: /node_modules/,
+	      use: {
+	        loader: 'babel-loader',
+	        options: {
+	          presets: [
+	            [
+					'@babel/preset-env'
+				]
+	          ]
+	        }
+	      }
+	    }
+	  ]
+	},
+
+	mode: "production"
 }
