262d6fe73362296a2ef3363a95b2c31f03cbed2b
elijahr2411 <elijah@elijahr.dev>
2023-02-10 09:01:19 -0500

    revert common.js

--- src/common.js
+++ src/common.js
@@ -1,6 +1,14 @@
 export const config = {
     serverAddresses: [
-        "ws://127.0.0.1:6004",
+        "wss://computernewb.com/collab-vm/vm0",
+        "wss://computernewb.com/collab-vm/vm1",
+        "wss://computernewb.com/collab-vm/vm2",
+        "wss://computernewb.com/collab-vm/vm3",
+        "wss://computernewb.com/collab-vm/vm4",
+        "wss://computernewb.com/collab-vm/vm5",
+        "wss://computernewb.com/collab-vm/vm6",
+        "wss://computernewb.com/collab-vm/vm7",
+        "wss://computernewb.com/collab-vm/vm8",
     ],
     chatSound: "https://computernewb.com/collab-vm/notify.ogg",
     // What XSS implementation the server uses
